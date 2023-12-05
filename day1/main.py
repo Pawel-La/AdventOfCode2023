f43e356bdffc53d2a1cbccf3c503ea1322d1b0ca
Paweł Lamża <103361216+Pawel-La@users.noreply.github.com>
2023-12-05 20:31:27 +0100

    Add files via upload

---
+++ day1/main.py
@@ -0,0 +1,29 @@
+def task1(y):
+    f = lambda l: list(filter(lambda x: x.isdigit(), l))
+    return sum(int(f(l)[0] + f(l)[-1]) for l in y)
+
+
+def task2(i):
+    import re
+
+    def g(l):
+        n = ["one", "two", "three", "four", "five", "six", "seven", "eight",
+             "nine"]
+        x = re.findall(f'(?=(\d|{"|".join(n)}))', l)
+
+        f = lambda a: str(n.index(a) + 1) if a in n else a
+
+        return int(f(x[0]) + f(x[-1]))
+
+    return sum(g(o) for o in i)
+
+
+def read_input():
+    with open('day1/input.txt', 'r') as f:
+        return f.read().splitlines()
+
+
+if __name__ == '__main__':
+    lines = read_input()
+    task1(lines)
+    task2(lines)
