6d600b79c80c9797200afd4274f7982d114a3176
Paweł Lamża <103361216+Pawel-La@users.noreply.github.com>
2023-12-05 20:33:23 +0100

    Add files via upload

---
+++ template.py
@@ -0,0 +1,17 @@
+def task1(lines):
+    return 0
+
+
+def task2(lines):
+    return 0
+
+
+def read_input():
+    with open('dayX/input.txt', 'r') as f:
+        return f.read().splitlines()
+
+
+if __name__ == '__main__':
+    lines = read_input()
+    task1(lines)
+    task2(lines)
