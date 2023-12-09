15e37016845824c382498d6eb2db10e10462c567
Paweł <pawellamza@gmail.com>
2023-12-09 10:01:13 +0100

    added day8 task 1.

--- day8/main.py
+++ day8/main.py
@@ -1,9 +1,49 @@
+import re
+
+
 def task1(lines):
-    return 0
+    d = {}
+    dirs = ""
+
+    for i, line in enumerate(lines):
+        if i == 0:
+            dirs = [0 if c == "L" else 1 for c in line]
+        if i >= 2:
+            [a, b, c] = re.findall(r'\w+', line)
+            d[a] = (b, c)
+
+    word = "AAA"
+    steps = 0
+
+    while word != "ZZZ":
+        dir = dirs[steps % len(dirs)]
+        word = d[word][dir]
+        steps += 1
+
+    return steps
 
 
 def task2(lines):
-    return 0
+    d = {}
+    dirs = ""
+
+    for i, line in enumerate(lines):
+        if i == 0:
+            dirs = [0 if c == "L" else 1 for c in line]
+        if i >= 2:
+            [a, b, c] = re.findall(r'\w+', line)
+            d[a] = (b, c)
+
+    words = list(filter(lambda key: key[-1] == "A", d.keys()))
+    print(words)
+    # steps = 0
+    #
+    # while word != "ZZZ":
+    #     dir = dirs[steps % len(dirs)]
+    #     word = d[word][dir]
+    #     steps += 1
+
+    # return steps
 
 
 def read_input():
@@ -13,5 +53,6 @@ def read_input():
 
 if __name__ == '__main__':
     lines = read_input()
-    task1(lines)
+    # print(task1(lines))
     task2(lines)
+    # print([0 if x == "a" else 1 for x in "abc"])
